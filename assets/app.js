2056ac00d40601fec32f76fd221665ffb01047eb
Tphorton14 <Tphorton14@gmail.com>
2019-07-30 08:59:34 -0400

    file update

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// store trive questions
+
+function gameQA () {
+    this.triviaQuest = {
+        ["Name the largest freshwater lake in the world?",["Lake Lanier","Lake Taho","Lake Superior","Lake Okeechobee"],1],
+        ["What is the diameter of Earth?",["8,000 Miles","11,275 Miles","6,898 Miles","11,111 Miles"],2],
+        ["Which garden is considered to be among the Seven Wonders of the Ancient World?",["Spring Gardens Keukenhof","Gardens of Versailles","The Hanging Gardens of Babylon","Royal Botanic Gardens at Kew"],3],
+        ["In 2011, which country hosted a Formula 1 race for the first time?",["Chile","Poland","England","India"],4],
+        ["In what three cities did the Braves win the 1914, 1957, and 1995 World Series?",["Chicago, Boston, Atlanta","Boston, Atlanta, Milwaukee","Milwaukee, Atlanta, Brooklyn","Atlanta, San Francisco, Cleveland"],5],
+        ["Who was Dan Reeves successor as head coach after Reeves was fired with a few games left in 2003?",["Wade Phillips","Bobby Petrino","Mike Smith","Jim L. Mora"],6],
+        ["What number did Atlanta United retire for the fans?",["1","17","7","18"],7],
+        ["What is my favorite number?",["8","7","24","16"],8],]
+    }
+}
